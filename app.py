14874a00e6b42f51b7d7edd521929783b012c8cf
Brian Lam <iBriaan@gmail.com>
2013-01-20 19:26:38 -0500

    Raymond needs to stop writing my git commit message
    Added sheets.html because it wasn't added from room 307 when we
    committed there, added credits.html. Beautified the code and had all
    pages on a consistent CSS theme.

--- app.py
+++ app.py
@@ -5,12 +5,28 @@ from flask import render_template
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET", "POST"])
-def home():
-    return render_template("Canvas.html")
-
-@app.route("/index.html", methods = ["GET", "POST"])
 def index():
-    return render_template("index.html")
+    return render_template("home.html")
+
+@app.route("/home.html", methods = ["GET", "POST"])
+def generic():
+    return render_template("home.html")
+
+@app.route("/whiteboard.html", methods = ["GET", "POST"])
+def whiteboard():
+    return render_template("whiteboard.html")
+
+@app.route("/sheets.html", methods = ["GET", "POST"])
+def sheets():
+    return render_template("sheets.html")
+
+@app.route("/credits.html", methods = ["GET", "POST"])
+def credits():
+    return render_template("credits.html")
+
+@app.route("/<name>", methods = ["GET", "POST"])
+def home():
+    return render_template("home.html")
 
 if __name__ == '__main__':
     app.run(debug = True)
