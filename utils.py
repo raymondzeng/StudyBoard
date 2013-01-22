339e4711c72cdd9510ebe8e86281e4fa4d23c07c
Brian Lam <iBriaan@gmail.com>
2013-01-22 00:27:37 -0500

    The python shelve should update now

--- utils.py
+++ utils.py
@@ -1,16 +1,20 @@
 import shelve
 
-sheets = shelve.open("sheets")
-
 def addsheet(title, tags, data):
+    sheets = shelve.open("sheets")
     data = {"ti":title, "ta":tags, "da":data}
     sheets[str(title)] = data
+    sheets.close()
     return True
 
 def returnsheets():
+    sheets = shelve.open("sheets")
     keys = sheets.keys()
     print "Here are your keys " + str(keys)
     a = []
     for key in keys:
         a.append(sheets[key])
+    sheets.close()
     return a
+
+
