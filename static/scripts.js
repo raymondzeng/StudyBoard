19bfab5f6114cbd598da807df0ad02c27b99b72c
Dexter Tong <schoolpurposes@gmail.com>
2013-01-23 01:09:01 -0500

    added banner, someone fix spacing

--- static/scripts.js
+++ static/scripts.js
@@ -19,6 +19,14 @@ function hideStuff(id) {
 
 function init()
 {
+    var sw = $(window).width();
+    var sp = 605;
+    while(sw > 0){
+	$("#bannercenter").append("<img src='static/images/bannercenter.png' style='position:absolute;top:0px;left:" + sp.toString() + "px'/>");
+	sp += 5;
+	sw -= 5;
+    }
+
     canvas1=document.getElementById('can');
     ctx1=canvas1.getContext("2d");
     w=canvas1.width;
